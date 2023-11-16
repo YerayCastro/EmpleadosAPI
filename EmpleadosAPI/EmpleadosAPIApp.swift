84d756ec62397a45c2734941a084eb8d3639c2e3
YerayCastro <112658475+YerayCastro@users.noreply.github.com>
2023-11-16 17:52:44 +0100

    Initial Commit

---
+++ EmpleadosAPI/EmpleadosAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmpleadosAPIApp.swift
+//  EmpleadosAPI
+//
+//  Created by Yery Castro on 16/11/23.
+//
+
+import SwiftUI
+
+@main
+struct EmpleadosAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
